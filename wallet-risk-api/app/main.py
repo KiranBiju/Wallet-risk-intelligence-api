e5f398e231cf1efa20a0bf92de3fd76a709c64f4
Kiran Biju <kiranbijun@gmail.com>
2026-04-09 00:38:14 +0530

    Updated FastAPI with ML connection, Data retrieval and Logging

--- wallet-risk-api/app/main.py
+++ wallet-risk-api/app/main.py
@@ -1,18 +1,67 @@
-from fastapi import FastAPI
+import logging
+import logging.config
+import time
+from fastapi import FastAPI, HTTPException
 from schemas.risk_schema import WalletRequest
+from services.risk_service import assess_wallet_risk
+
+# setup loggers
+logging.basicConfig(level=logging.INFO)
+
+# get root logger
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+
+    logger.info("logging from the root logger")
+
+    return {"status": "running"}
 
 @app.get("/health")
 def health():
-    return{"status": "ok"}
+    return{"status": "success"}
 
 @app.post("/risk/score")
 async def score_wallet(request: WalletRequest):
-    wallet = request.wallet
-    return {"wallet": wallet}    
+
+    start_time = time.time() 
+    
+    try:
+        wallet = request.wallet
+
+        logger.info(f"Received wallet: {wallet}")
+        logger.info("Starting wallet risk assessment")
+
+        features = {
+        "tx_frequency": 10,
+        "avg_tx_value": 2
+    }
+        result = assess_wallet_risk(wallet, features)
+
+        logger.info("Risk assessment completed successfully")
+
+        return {
+            "status": "success",
+            "data": result
+        }
+
+    except Exception as e:
+
+        logger.error(f"Error processing wallet: {str(e)}", exc_info=True)
+
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    finally:
+        end_time = time.time()
+        execution_time = end_time - start_time
+
+        logger.info(f"Execution time: {execution_time:.2f} seconds")
+
+
+        
+
+    
